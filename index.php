400647f4fbbc1bd7699dd7fb72e1b70f785dfd6b
gg-mg <ggichuki2017@gmail.com>
2020-01-19 19:30:38 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+<h1>Testing the Post Method</h1>
+
+<form action="post_method.php" method="post">
+    
+<input type="text" name="username" placeholder="Enter Username">
+<input type="password"  name="password" placeholder="Enter Password"><br>
+<input type="submit" name="submit">
+    
+    
+</form>
+
+
+
+</body>
+</html>
